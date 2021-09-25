7dfcfb36c941f712390155429936c1c3bad62c4d
Matthew Kimball <mekimba6@gmail.com>
2021-09-25 14:06:33 -0600

    adds letter to braille module

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,1 @@
-this is a bunch of text
-well not that much
-but still some
+a
--- runner.rb
+++ runner.rb
@@ -1,11 +1,12 @@
+require './lib/dictionary'
+
 first_arg, second_arg = ARGV
 first_lines = File.readlines(first_arg)
 
-new_file = File.open(second_arg, 'w') do |f|
+new_file = File.open(second_arg, 'w') do |_f|
   IO.copy_stream(first_arg, second_arg)
-  p ARGV
-  p second_arg
 end
+
 second_lines = File.readlines(second_arg)
 line_count = second_lines.size
 text = second_lines.join
