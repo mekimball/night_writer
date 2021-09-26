92597f5e349b5eef08b5356b523e0d5b5b46b4d7
Matthew Kimball <mekimba6@gmail.com>
2021-09-26 17:04:32 -0600

    adds cross file compatability with braille translate

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -3,14 +3,12 @@ require_relative 'dictionary'
 class BrailleWriter
   include Dictionary
 
-
   def translate(characters)
     characters = characters.split('')
-   translate_array = []
-   characters.each do |character|
-      # require 'pry'; binding.pry
+    translate_array = []
+    characters.each do |character|
       if character.ord.between?(48, 57)
-       translate_array << number_keys['#']
+        translate_array << number_keys['#']
         translate_array << number_keys[character]
       else
         translate_array << letter_keys[character]
@@ -18,13 +16,7 @@ class BrailleWriter
     end
     translate_array
   end
-
-  # def print_chars(chars)
-  #   characters = translate(chars)
-  #   # require 'pry'; binding.pry
-  #   characters
-  # end
-
+  
   def translate_back(chars)
     chars.map do |char|
       braille_keys[char]
